42a94ac2ffb3a2545ab6c5d97d7501c6bcd69bce
Lokeshwar Reddy <nlreddycode@gmail.com>
2021-12-29 17:48:18 -0800

    updated dialog box

--- battery_alert.py
+++ battery_alert.py
@@ -1,28 +1,41 @@
 import psutil
 import time
 import tkinter
-st=int(input("Enter start time"))
-et=int(input("Enter end time"))
+
+low=int(input("Enter lowest battery percentage : "))
+high=int(input("Enter highest battery percentage : "))
+
 battery = psutil.sensors_battery()
-def alert_window():
+
+def alert_window(battery_value):
 	t=tkinter.Tk()
+	t.geometry("200x200")
+
+	if battery_value==0:
+		alert_label=tkinter.Label(t,text="Low battery").place(x=70,y=90)
+	else:
+		alert_label=tkinter.Label(t,text="Charged").place(x=70,y=90)
 	t.mainloop()
+
 import _thread as thread
+
 while(1):
-	if battery.percent<st and battery.power_plugged==False:
+	if battery.percent<low and battery.power_plugged==False:
 		print("\a")
-		thread.start_new_thread(alert_window,())
+		thread.start_new_thread(alert_window,(0,))
 		while(battery.power_plugged==False):
 			print("\a")
 			time.sleep(2)
 			battery = psutil.sensors_battery()
-	elif battery.percent>et and battery.power_plugged==True:
+
+	elif battery.percent>high and battery.power_plugged==True:
 		print("\a")
-		thread.start_new_thread(alert_window,())
+		thread.start_new_thread(alert_window,(1,))
 		while(battery.power_plugged==True):
 			print("\a")
 			time.sleep(2)
 			battery = psutil.sensors_battery()
+
 	else:
 		time.sleep(30)
 	battery = psutil.sensors_battery()
