638e472dc090ed815b6bffda798740e9231e9ca9
Lokeshwar Reddy <54455679+reddylokeshwar555@users.noreply.github.com>
2024-02-08 18:02:01 -0600

    Update battery_alert.py

--- battery_alert.py
+++ battery_alert.py
@@ -1,42 +1,38 @@
 import psutil
-import time
 import tkinter
-
-low=int(input("Enter lowest battery percentage : "))
-high=int(input("Enter highest battery percentage : "))
-
-battery = psutil.sensors_battery()
+from threading import Thread
+import time
 
 def alert_window(battery_value):
-	t=tkinter.Tk()
-	t.geometry("200x200")
+    t = tkinter.Tk()
+    t.geometry("200x200")
+    if battery_value == 0:
+        alert_label = tkinter.Label(t, text="Low battery").place(x=70, y=90)
+    else:
+        alert_label = tkinter.Label(t, text="Charged").place(x=70, y=90)
+    t.mainloop()
 
-	if battery_value==0:
-		alert_label=tkinter.Label(t,text="Low battery").place(x=70,y=90)
-	else:
-		alert_label=tkinter.Label(t,text="Charged").place(x=70,y=90)
-	t.mainloop()
+def battery_monitor(low, high):
+    while True:
+        battery = psutil.sensors_battery()
+        if battery.percent < low and not battery.power_plugged:
+            print("\a")
+            Thread(target=alert_window, args=(0,)).start()
+            while not battery.power_plugged:
+                print("\a")
+                time.sleep(2)
+                battery = psutil.sensors_battery()
+        elif battery.percent > high and battery.power_plugged:
+            print("\a")
+            Thread(target=alert_window, args=(1,)).start()
+            while battery.power_plugged:
+                print("\a")
+                time.sleep(2)
+                battery = psutil.sensors_battery()
+        else:
+            time.sleep(30)
 
-import _thread as thread
-
-while(1):
-	if battery.percent<low and battery.power_plugged==False:
-		print("\a")
-		thread.start_new_thread(alert_window,(0,))
-		while(battery.power_plugged==False):
-			print("\a")
-			time.sleep(2)
-			battery = psutil.sensors_battery()
-
-	elif battery.percent>high and battery.power_plugged==True:
-		print("\a")
-		thread.start_new_thread(alert_window,(1,))
-		while(battery.power_plugged==True):
-			print("\a")
-			time.sleep(2)
-			battery = psutil.sensors_battery()
-
-	else:
-		time.sleep(30)
-	battery = psutil.sensors_battery()
-
+if __name__ == "__main__":
+    low = int(input("Enter lowest battery percentage : "))
+    high = int(input("Enter highest battery percentage : "))
+    battery_monitor(low, high)
